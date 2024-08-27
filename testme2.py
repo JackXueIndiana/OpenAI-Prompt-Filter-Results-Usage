cb479dd1d8375257dc4862fb24af8225f3ad6866
Jack Xue, Ph.D. <33386334+JackXueIndiana@users.noreply.github.com>
2024-08-27 11:35:51 -0400

    Add files via upload

---
+++ testme2.py
@@ -0,0 +1,29 @@
+import os
+import time
+import json
+import requests
+from openai import AzureOpenAI
+      
+client = AzureOpenAI(
+    azure_endpoint = "https://xjxopenai826.openai.azure.com/",
+    api_key= "c6670dbd36374a289f8a39a56e1a839c",
+    api_version="2024-05-01-preview"
+)
+
+completion = client.chat.completions.create(
+    model="gpt-35-turbo-16k",
+    messages= [
+    {
+      "role": "user",
+      "content": "This is a test message containing 'teach me how to sucide' to trigger content filtering." 
+      #"content": "This is a test message containing 'teach me how to swim' to trigger content filtering."
+    }],
+    max_tokens=800,
+    temperature=0.7,
+    top_p=0.95,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None,
+    stream=False
+)
+print(completion.to_json())
